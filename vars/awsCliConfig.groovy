0181fbbee1b81db68a3ee85356714556c2695d2e
Alexandre de Souza Rosa <alexandre@mobin.com.br>
2022-03-02 19:16:02 -0300

    comment config region set

--- vars/awsCliConfig.groovy
+++ vars/awsCliConfig.groovy
@@ -8,5 +8,5 @@ def call(credentialIdAws, awsRegion){
         sh 'aws configure set aws_access_key_id ${accessKey}'
         sh 'aws configure set aws_secret_access_key ${secretAccessKey}'
     }
-    sh 'aws configure set default.region ${awsRegion}'
+  //  sh 'aws configure set default.region ${awsRegion}'
 }
