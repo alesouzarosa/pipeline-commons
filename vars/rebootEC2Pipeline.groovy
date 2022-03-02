bfc18eba2897d808e41a427b44fc1619a112d29f
Alexandre de Souza Rosa <alexandre@mobin.com.br>
2022-03-02 16:38:01 -0300

    excluidor o scheduler

--- vars/rebootEC2Pipeline.groovy
+++ vars/rebootEC2Pipeline.groovy
@@ -3,7 +3,7 @@
 def call(body) {
 
     def pipelineParams= [
-            schedule: "0 0 0 0 0"   /*cron format */,
+            
             credentialIdAws: "AWS_JENKINS_CREDENTIALS",
             instanceId: "",
             awsRegion: "us-east-1"
