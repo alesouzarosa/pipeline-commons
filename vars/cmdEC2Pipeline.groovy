c563e6a1cf1588eb794fe86eba85e861fbfd56ef
Alexandre de Souza Rosa <alexandre@mobin.com.br>
2022-03-03 19:08:47 -0300

    add cmdEC2Pipeline

---
+++ vars/cmdEC2Pipeline.groovy
@@ -0,0 +1,57 @@
+!groovy
+
+def call(body) {
+
+    def pipelineParams= [
+            objeto:             
+            schedule: "0 12,20 */1 * *",
+            credentialIdAws: "AWS_JENKINS_CREDENTIALS",
+            instanceId: "instanceid",
+            awsRegion: "us-east-1"
+    ]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    
+
+    pipeline {
+        agent any
+        triggers {
+            parameterizedCron(pipelineParams.object}
+ 
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '1'))
+            disableConcurrentBuilds()
+        }
+        stages {
+
+            stage("select a job"){
+                //deve ler a tabela de jobs e retorna o trablaho que deve ser feito
+
+
+            }
+
+            stage('credentialIn') {
+                steps{
+                    awsCliConfig(pipelineParams.credentialIdAws, pipelineParams.awsRegion)
+                }
+            }
+
+            stage('reboot') {
+                steps{
+                    awsCliReboot()
+                }
+            }
+        }
+        /*
+        post {
+            always {
+                script {
+                    slackNotifier(currentBuild.currentResult)
+                }
+            }
+        }
+        */
+    }
+}
