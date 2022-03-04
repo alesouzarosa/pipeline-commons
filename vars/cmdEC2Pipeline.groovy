5e26292dcc39ff79097ab32ef98f47e8a975cc2b
Alexandre de Souza Rosa <alexandre@mobin.com.br>
2022-03-04 15:53:36 -0300

    fddsfsd

--- vars/cmdEC2Pipeline.groovy
+++ vars/cmdEC2Pipeline.groovy
@@ -3,10 +3,9 @@
 def call(body) {
 
     def pipelineParams= [
-            objparameterizedCron:"""
-        */2 * * * * %GREETING=Hola;PLANET=plutaoooo
-        */4 * * * * %PLANET=Marterrr
-        """
+            objparameterizedCron: """*/2 * * * * %GREETING=Hola;PLANET=plutaoooo
+                */4 * * * * %PLANET=Marterrr
+                """
     ]
 
     body.resolveStrategy = Closure.DELEGATE_FIRST
