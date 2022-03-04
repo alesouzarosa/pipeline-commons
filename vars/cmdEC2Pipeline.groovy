1baeb293fef12e75bd3ad174e108818939943bb4
Alexandre de Souza Rosa <alexandre@mobin.com.br>
2022-03-04 14:52:37 -0300

    czxczxc

--- vars/cmdEC2Pipeline.groovy
+++ vars/cmdEC2Pipeline.groovy
@@ -7,10 +7,7 @@ def call(body) {
 
              //String """___"""  
 
-            objparameterizedCron:"""
-                */2 * * * * %GREETING=Hola;PLANET=plutaoooo
-                */4 * * * * %PLANET=Marterrr
-                """ ,
+            objparameterizedCron,
 
             credentialIdAws: "AWS_JENKINS_CREDENTIALS"
 
@@ -31,21 +28,6 @@ def call(body) {
         triggers {
             parameterizedCron(pipelineParams.objparameterizedCron)
         }
-
-
-        // parameters{[
-        //     string(name: 'instaceId', defaultValue: 'i-123456789', description: 'Which planet are we on?'),
-        //     string(name: 'awsRegion', defaultValue: 'us-east-1', description: 'Which planet are we on?'),
-        //     string(name: 'command', defaultValue: 'Hello', description: 'How shall we greet?')   
-        // ]}
-        // triggers {
-        //     parameterizedCron("""
-        //         */2 * * * * %GREETING=Hola;PLANET=Pluto
-        //         */3 * * * * %PLANET=Mar
-        //     """)
-        // }
-
-
         options {
             buildDiscarder(logRotator(numToKeepStr: '1'))
             disableConcurrentBuilds()
